684ec67878b5d3b97b0ae692d2cc373a3a8d2d43
asantosmanu <manuelasantos452006@gmail.com>
2023-07-22 14:19:14 -0300

    fznd dicas

--- src/views/DicaView1.vue
+++ src/views/DicaView1.vue
@@ -1,5 +1,12 @@
 <template>
 
-    <p>texto da dica 1 AQUI</p>
+    <p>O evento que desencadeou o início da Primeira Guerra Mundial foi o assassinato
+do arquiduque Franz Ferdinand, herdeiro do trono do Império Austro -Húngaro, em Sarajevo, no
+dia 28 de junho de 1914 (MARTINS, 2006). Esse evento foi o estopim para uma série de conflitos
+e tensões diplomáticas entre as potências europeias, que culminaram na eclosão do conflito em
+agosto daquele ano (MARTINS, 2006). A morte de Franz Ferdinand foi planejada por
+nacionalistas sérvios, que desejavam a independência de seu país em relação à Áustria-Hungria
+(MARTINS, 2006). Esse fato levou à declaração de guerra da Áustria-Hungria à Sérvia, e
+posteriormente, à entrada de outras potências na guerra (MARTINS, 2006).</p>
     
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,8 +11,23 @@
         <input class="border" type="radio" v-on:keypress="ok = false" v-model="h1" value="OPCOES" /> OP2
         <button @click="$router.push({name: 'dica2'})"> Dica</button>
         <input class="border" type="radio" v-on:keypress="ok = false" v-model="h1" value="OPCOES" /> OP3
+        <button @click="$router.push({name: 'dica3'})"> Dica</button>
         <input class="border" type="radio" v-on:keypress="ok = false" v-model="h1" value="OPCOES" /> OP4
+        <button @click="$router.push({name: 'dica4'})"> Dica</button>
         <input class="border" type="radio" v-on:keypress="ok = false" v-model="h1" value="OPCOES" /> OP5
+        <button @click="$router.push({name: 'dica5'})"> Dica</button>
+        <input class="border" type="radio" v-on:keypress="ok = false" v-model="h1" value="OPCOES" /> OP6
+        <button @click="$router.push({name: 'dica6'})"> Dica</button>
+        <input class="border" type="radio" v-on:keypress="ok = false" v-model="h1" value="OPCOES" /> OP7
+        <button @click="$router.push({name: 'dica7'})"> Dica</button>
+        <input class="border" type="radio" v-on:keypress="ok = false" v-model="h1" value="OPCOES" /> OP8
+        <button @click="$router.push({name: 'dica8'})"> Dica</button>
+        <input class="border" type="radio" v-on:keypress="ok = false" v-model="h1" value="OPCOES" /> OP9
+        <button @click="$router.push({name: 'dica9'})"> Dica</button>
+        <input class="border" type="radio" v-on:keypress="ok = false" v-model="h1" value="OPCOES" /> OP10
+        <button @click="$router.push({name: 'dica10'})"> Dica</button>
+        <input class="border" type="radio" v-on:keypress="ok = false" v-model="h1" value="OPCOES" /> OP11
+        <button @click="$router.push({name: 'dica11'})"> Dica</button>
        <hr>
         <button @click="$router.push({name: 'about'})">Ir para sobre</button>
   </div>
