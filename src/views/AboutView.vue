1165d0c09cb9e4cbd85374007d890aa5a0c239f3
asantosmanu <manuelasantos452006@gmail.com>
2023-07-23 00:19:49 -0300

    tentativa botao pergunta

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,13 @@
 <template>
   <h1>ENTRADA</h1>
-  <P>O que é, qnts perguntas......</P>
-  <button @click="$router.push({name: 'home'})">Ir para home</button>
+  <P>Bem-vindo ao nosso empolgante Quiz, onde desafiamos seus conhecimentos em história! Se você é apaixonado por curiosidades, um mestre da história ou apenas busca diversão e aprendizado, este é o lugar certo para você!
+
+Prepare-se para testar suas habilidades, responder perguntas intrigantes e descobrir o quanto você realmente sabe sobre os mais variados assuntos envolvendo história, estamos prontos para desafiá-lo em todos os níveis!
+
+Não importa se você é um novato ou um especialista em trivia, nosso Quiz é projetado para ser uma experiência divertida e educativa para todos. dirvirta-se, supere seus recordes e desafie seus amigos para ver quem possui o conhecimento mais abrangente.
+
+Então, ajuste seus óculos, ligue o cérebro e mergulhe nesta jornada intelectual. Vamos começar o Quiz e descobrir quão bem você realmente conhece a história! Boa sorte!</P>
+  <button @click="$router.push({name: 'home'})">Ir para o quiz</button>
 </template>
 
 <style></style>
